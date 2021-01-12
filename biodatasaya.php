e01d27eb860dfa98761c3f7aa134f6c08a31de3f
kael <dimaspraditya.dp@gmail.com>
2021-01-12 20:22:53 +0700

    biosaya1

---
+++ biodatasaya.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Latihan 1</title>
+    </head>
+    <body>
+        <h1>Haii Halloooo erkenalkan nama saya Mikael Dimas Praditya Sarjono</h1>
+        <h1>Saya lulusan Atma jaya Yogyakarta, Fakultas Teknologi Industri, Prodi Teknik Informatika</h1>
+        <h1>hobi saya adalah:</h1>
+        <h2>1. masak</h2>
+        <h2>2. bermain game</h2>
+        <h2>3. berenang</h2>
+        <h2>4. traveling</h2>
+        <h1>Tujuan saya mengikuti bootcamp adalah:</h1>
+        <h2>1. saya ingin mengenal lebih dalam tentang dunia pemograman</h2>
+        <h2>2. saya ingin memiliki pekerjaan yang berbasis pemogramman</h2>
+        <h2>3. memiliki karir yang lebih baik kedepannya</h2>
+        <h2>4. memiliki banyak konkesi (teman)</h2>
+    </body>
+</html>
